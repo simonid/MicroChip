2313c435b65f019c40fa2b4d60e08ea741d86fab
simonid <woshisimonchen@gmail.com>
2017-10-22 19:53:21 +0800

    修改127-8

--- MCS-51/P127-8/127-8.c
+++ MCS-51/P127-8/127-8.c
@@ -1,30 +1,25 @@
 #include "reg51.h"
-sbit P3_0 = P3^0;
+sbit P1_1=P1^1;
 
-
-/*
-void delay(unsigned int time){
- 	unsigned int j = 0;
-	for(;time>0;time--)
-	for(j=0;j<250;j++);
-}	*/
-void main(){
-	int n=0;
-	TMOD = 0x05;	 //T0计数模式，方式1
+void main(){	 
+	TMOD = 0x05;	 //T0计数模式，方式1	
+	TH0=0Xff;		  
+	TL0=0xf6; //10个
 	TR0 = 1;		 //开启计数器0
-	TL0=0Xff;		  //一个外来脉冲来了之后就溢出
-	while(1){
-		
-	 do{n++;}while(!TF0);		  //10个脉冲到来	
+	while(1){	
+	 do{} while(!TF0);		  //脉冲计数	
 		TF0=0; 	  //TF0复位
-		while(n==10);//等待10个脉冲		
-		n=0;					
+		P1_1=0;					
 		TMOD = 0x01;						   //T0定时方式1
-		TH0 = (65536-1000*2)/256;				//赋初值，1ms
-		TL0 = (65536-1000*2)%256;
+		TH0 = 0xfe;				//赋初值，1ms
+		TL0 = 0x0c;
 		while(!TF0);							//等待定时溢出
-		TF0=0;								  //TF0复位
+		TF0=0;
+		P1_1=1;								  //TF0复位
 		TMOD = 0x05;						   //重新设置T0计数模式1
-		TL0=0xff;								 //赋予初值
+		TH0=0xff;								 //赋予初值
+		TL0=0xf6;
+
 	}
 }
+
